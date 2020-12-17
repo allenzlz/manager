841b0395613ae642e93802fe234853466cb01de2
zhenglongzhen <2532840901@qq.com>
2020-12-17 14:05:45 +0800

    解决iframe不显示问题

--- src/main/java/com/nb/manager/config/WebSecurityConfig.java
+++ src/main/java/com/nb/manager/config/WebSecurityConfig.java
@@ -91,6 +91,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .tokenRepository(persistentTokenRepository())//设置持久化token
                 .tokenValiditySeconds(24 * 60 * 60); //记住登录1天(24小时 * 60分钟 * 60秒)
 
+        http.headers().frameOptions().sameOrigin();// 同源跨域
+
         //关闭csrf跨域攻击防御
         http.csrf().disable();
 
